f293c9c997a7becaf1870d9da4a0a9be5d91f50f
gauravbajaj27 <gauravbajaj2710@gmail.com>
2021-07-23 15:55:51 +0530

    Course Form Added

--- Student_Companion/courses/views.py
+++ Student_Companion/courses/views.py
@@ -1,3 +1,9 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
+from .forms import courseForm
 
 # Create your views here.
+def addCourse(request):
+    form = courseForm()
+    return render(request,'courses/courseForm.html',{'form':form})
