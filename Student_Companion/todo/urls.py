ee8294550144548c9dbd4c7ffd7d50bcda5612fe
Jimmy290901 <shubham290901@gmail.com>
2021-07-30 18:18:52 +0530

    Fixed datetimepicker and added task delete functionality

--- Student_Companion/todo/urls.py
+++ Student_Companion/todo/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', listTasks),
     path('updateTask/<int:task_id>', updateTask, name="updateTask"),
-    path('deleteTask/<int:task_id>',deleteTask, name="deleteTask"),
+    path('deleteTask/<int:task_id>', deleteTask, name="deleteTask"),
 ]
--- Student_Companion/todo/views.py
+++ Student_Companion/todo/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .forms import *
 from .models import *
 from users.models import Person
+from django.core.exceptions import PermissionDenied
 # Create your views here.
 
 def listTasks(request):
     newTask = NewTaskForm()
-    person = Person.objects.get(user=request.user)
+    person = get_object_or_404(Person, user=request.user)
     if request.method == 'POST':
         task = NewTaskForm(request.POST)
         if task.is_valid():
@@ -14,7 +15,7 @@ def listTasks(request):
             created_task.person = person
             created_task.save()
             print('Task created successfully.')
-    tasksList = Tasks.objects.filter(person=person)
+    tasksList = Tasks.objects.filter(person=person).order_by('-d_and_t')
     context = {
         'form':newTask,
         'tasksList': tasksList,
@@ -25,4 +26,9 @@ def updateTask(request, task_id):
     print(task_id)
 
 def deleteTask(request, task_id):
-    print(task_id)
+    person = get_object_or_404(Person, user = request.user)
+    task = get_object_or_404(Tasks, id = task_id)
+    if person != task.person:
+        raise PermissionDenied
+    task.delete()
+    return redirect('/todo')
