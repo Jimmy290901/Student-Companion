e8fb2880f422ece548ee32f6c7f564c7fc9ba78e
Jimmy290901 <shubham290901@gmail.com>
2021-08-19 18:27:22 +0530

    Finished Course Instructors sub-pg

---
+++ Student_Companion/static/courses/course_script.js
@@ -0,0 +1,19 @@
+
+
+var active_tab = "overview";
+
+function show(tab) {
+    const curr_tab = document.getElementById(active_tab.concat("_tab"));
+    const select_tab = document.getElementById(tab.concat('_tab'));
+
+    const curr_block = document.getElementById(active_tab.concat("_block"));
+    const select_block = document.getElementById(tab.concat("_block"));
+    
+    curr_block.classList.add("no-display");
+    select_block.classList.remove("no-display");
+    
+    curr_tab.classList.remove("tab-hghl");
+    select_tab.classList.add("tab-hghl");
+    
+    active_tab = tab;
+}
